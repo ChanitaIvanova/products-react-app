2aa4c0a70a327e5f81c62b8efa36cc3187fb15b9
Chanita T. Ivanova <chanita.ivanova@MUSALA.COM>
2020-08-03 16:33:57 +0300

    Products reducer unit tests

---
+++ client/src/state/products/products.test.js
@@ -0,0 +1,58 @@
+import { Products } from './products'
+import { RECEIVE_PRODUCTS, REQUEST_PRODUCTS } from '../actionTypes';
+
+describe('Products', () => {
+    it('should return the initial state', () => {
+        expect(Products(undefined, {})).toEqual({ products: [], areLoading: false });
+    });
+
+    it('should handle RECEIVE_PRODUCTS', () => {
+        const products = [
+            {
+                id: 1,
+                name: "Iron",
+                price: 20,
+                currency: "USD"
+            },
+            {
+                id: 2,
+                name: "TV",
+                price: 500,
+                currency: "USD"
+            }
+        ];
+        expect(Products(undefined, { 
+                type: RECEIVE_PRODUCTS,
+                payload: products
+            })).toEqual({ products: products, areLoading: false });
+        
+        expect(Products({ products: [], areLoading: true }, { 
+            type: RECEIVE_PRODUCTS,
+            payload: products
+        })).toEqual({ products: products, areLoading: false });
+    });
+
+    it('should handle REQUEST_PRODUCTS', () => {
+        const products = [
+            {
+                id: 1,
+                name: "Iron",
+                price: 20,
+                currency: "USD"
+            },
+            {
+                id: 2,
+                name: "TV",
+                price: 500,
+                currency: "USD"
+            }
+        ];
+        expect(Products(undefined, { 
+                type: REQUEST_PRODUCTS
+            })).toEqual({ products: [], areLoading: true });
+        
+        expect(Products({ products: products, areLoading: false }, { 
+            type: REQUEST_PRODUCTS
+        })).toEqual({ products: products, areLoading: true });
+    });
+});
